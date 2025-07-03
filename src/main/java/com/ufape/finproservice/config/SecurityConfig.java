59e583747ab41242c4e0eb3849fe6cba30a01f38
izabel <izabel.nascimento@nees.ufal.br>
2025-07-02 22:52:23 -0300

    feat: Módulo de Autenticação e Autorização

---
+++ src/main/java/com/ufape/finproservice/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.ufape.finproservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
+                        .requestMatchers("/user/create")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
--- src/main/java/com/ufape/finproservice/service/UserService.java
+++ src/main/java/com/ufape/finproservice/service/UserService.java
@@ -5,6 +5,7 @@ import com.ufape.finproservice.mapper.UserMapper;
 import com.ufape.finproservice.model.User;
 import com.ufape.finproservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,42 +13,14 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public User save(UserDTO userDTO) {
         if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
             throw new RuntimeException("E-mail já está em uso");
         }
+        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         User user = UserMapper.toEntity(userDTO);
         return userRepository.save(user);
     }
 }
-//
-//package com.ufape.finproservice.service;
-//
-//import com.ufape.finproservice.dto.UserDTO;
-//import com.ufape.finproservice.mapper.UserMapper;
-//import com.ufape.finproservice.model.User;
-//import com.ufape.finproservice.repository.UserRepository;
-//import lombok.AllArgsConstructor;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class UserService {
-//
-//    private final UserRepository userRepository;
-//    private final BCryptPasswordEncoder passwordEncoder;
-//
-//    public User save(UserDTO userDTO) {
-//        if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
-//            throw new RuntimeException("E-mail já está em uso");
-//        }
-//        userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
-//        User user = UserMapper.toEntity(userDTO);
-//        return userRepository.save(user);
-//    }
-//}
-
-
-

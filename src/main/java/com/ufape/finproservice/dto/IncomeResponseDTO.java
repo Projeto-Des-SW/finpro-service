87224a33906c0f5489e23cb4cea075715631f6b5
mymph <leticialivia7991@gmail.com>
2025-07-28 23:17:03 -0300

    feat: adiciona IncomeResponseDTO para resposta de dados de Receita

---
+++ src/main/java/com/ufape/finproservice/dto/IncomeResponseDTO.java
@@ -0,0 +1,22 @@
+package com.ufape.finproservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class IncomeResponseDTO {
+    private Long incomeId;
+    private LocalDate date;
+    //private IncomeCategoryResponseDTO category;
+    private BigDecimal amount;
+    private String paymentOrigin;
+    private String balanceSource;
+    private String observation;
+    private UserResponseDTO user; //ou private Long userId (apenas o ID)
+}

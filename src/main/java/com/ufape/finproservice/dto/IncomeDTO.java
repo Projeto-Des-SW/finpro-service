dd2333992dd06806340d276cab0ed7c696ae076b
mymph <leticialivia7991@gmail.com>
2025-07-28 23:08:28 -0300

    feat: adiciona IncomeDTO para entrada de dados de Receita

---
+++ src/main/java/com/ufape/finproservice/dto/IncomeDTO.java
@@ -0,0 +1,21 @@
+package com.ufape.finproservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class IncomeDTO {
+    private LocalDate date;
+    private Long incomeCategoryId;
+    private BigDecimal amount;
+    private String paymentOrigin;
+    private String balanceSource;
+    private String observation;
+    private Long userId;
+}

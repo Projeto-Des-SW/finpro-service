39517aaac27e628e0cd940040ef7d54e3f303211
mymph <leticialivia7991@gmail.com>
2025-07-28 22:06:43 -0300

    refactor: ajusta ExpenseEntity com UUID e restrições de coluna

--- src/main/java/com/ufape/finproservice/model/ExpenseEntity.java
+++ src/main/java/com/ufape/finproservice/model/ExpenseEntity.java
@@ -5,27 +5,40 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "expense")
+@Table(name = "expenses")
 public class ExpenseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long expenseId;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID expenseId;
 
-    private String category;
-    private BigDecimal amount;
-    private String paymentDestination;
-    private String balanceSource;
+    @Column(nullable = false)
     private LocalDate date;
+
+    /*@ManyToOne
+    @JoinColumn(name = "expense_category_id")
+    private ExpenseCategoryEntity category;*/
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String paymentDestination;
+
+    @Column(nullable = false)
+    private String balanceSource;
+
+    @Column(columnDefinition = "TEXT")
     private String observation;
 
     @ManyToOne
-    @JoinColumn(name= "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 }

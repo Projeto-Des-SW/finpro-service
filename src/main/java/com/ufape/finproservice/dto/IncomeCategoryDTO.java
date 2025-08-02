9ad878ded271ff3ce7bb5b88701137cb66e1f38e
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-02 00:34:01 -0300

    Feat: DTOs IncomeCategory and Refactor: IncomeCategoryDto

---
+++ src/main/java/com/ufape/finproservice/dto/IncomeCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.ufape.finproservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class IncomeCategoryDTO {
+    private Long incomeCategoryId;
+    private String categoryName;
+}

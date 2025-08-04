a8778d35ffa9901eeb8bc188c7396544b481d6e3
mymph <leticialivia7991@gmail.com>
2025-08-04 20:26:07 -0300

    feat: criar service ExpenseCategoryService e adicionar mensagens de exceção

--- src/main/java/com/ufape/finproservice/exception/ExceptionMessage.java
+++ src/main/java/com/ufape/finproservice/exception/ExceptionMessage.java
@@ -15,10 +15,10 @@ public enum ExceptionMessage {
     EXPENSE_NOT_OWNED(403, "User doesn't own this expense"),
 
     INCOME_CATEGORY_ALREADY_EXISTS(400, "Income category already exists"),
-
     INCOME_CATEGORY_NOT_FOUND(404, "Income category not found"),
-
-    CATEGORY_HAS_INCOME(409, "Cannot delete income category that has associated incomes");
+    CATEGORY_HAS_INCOME(409, "Cannot delete income category that has associated incomes"),
+    EXPENSE_CATEGORY_NOT_FOUND(404, "Expense category not found"),
+    EXPENSE_CATEGORY_ALREADY_EXISTS(409, "Expense category already exists");
 
 
     private final int code;

1d0b370f415d408683da5b09f2e652b98dce41d0
mymph <leticialivia7991@gmail.com>
2025-08-04 19:40:25 -0300

    feat: criar DTO ExpenseCategoryDTO

---
+++ src/main/java/com/ufape/finproservice/dto/ExpenseCategoryDTO.java
@@ -0,0 +1,15 @@
+package com.ufape.finproservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpenseCategoryDTO {
+    private Long expenseCategoryId;
+    private String name;
+}

42068a3af93919226709dcdd4e00731d0f5261ef
mymph <leticialivia7991@gmail.com>
2025-08-04 22:51:36 -0300

    refactor: ajusta ExpenseMapper após criação de ExpenseCategory

--- src/main/java/com/ufape/finproservice/mapper/ExpenseMapper.java
+++ src/main/java/com/ufape/finproservice/mapper/ExpenseMapper.java
@@ -3,13 +3,14 @@ package com.ufape.finproservice.mapper;
 import com.ufape.finproservice.dto.ExpenseDTO;
 import com.ufape.finproservice.dto.ExpenseResponseDTO;
 import com.ufape.finproservice.model.Expense;
+import com.ufape.finproservice.model.ExpenseCategory;
 import com.ufape.finproservice.model.UserEntity;
 
 public class ExpenseMapper {
 
-    public static Expense toEntity(ExpenseDTO dto, UserEntity user) { //ExpenseCategoryEntity category
+    public static Expense toEntity(ExpenseDTO dto, UserEntity user, ExpenseCategory category) {
         if (dto == null) return null;
-
+        
         return Expense.builder()
                 .date(dto.getDate())
                 .amount(dto.getAmount())
@@ -17,13 +18,13 @@ public class ExpenseMapper {
                 .balanceSource(dto.getBalanceSource())
                 .observation(dto.getObservation())
                 .user(user)
-                //.category(category)
+                .expenseCategory(category)
                 .build();
     }
 
     public static ExpenseResponseDTO toExpenseResponseDTO(Expense expense) {
         if (expense == null) return null;
-
+        
         return ExpenseResponseDTO.builder()
                 .expenseId(expense.getExpenseId())
                 .date(expense.getDate())
@@ -32,8 +33,8 @@ public class ExpenseMapper {
                 .balanceSource(expense.getBalanceSource())
                 .observation(expense.getObservation())
                 .userId(expense.getUser().getId())
-                /*.category(expense.getCategory()) != null ?
-                    ExpenseCategoryMapper.toExpenseCategoryResponseDTO(expense.getCategory()) : null)*/
+                .category(expense.getExpenseCategory() != null ?
+                    ExpenseCategoryMapper.toExpenseCategoryResponseDTO(expense.getExpenseCategory()) : null)
                 .build();
     }
 }

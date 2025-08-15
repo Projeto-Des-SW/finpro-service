315040e8b94cecb7bd7d2af33cbcdb264c62d01b
mymph <leticialivia7991@gmail.com>
2025-08-14 22:51:28 -0300

    feat: query para gráfico de barras

--- src/main/java/com/ufape/finproservice/repository/IncomeRepository.java
+++ src/main/java/com/ufape/finproservice/repository/IncomeRepository.java
@@ -1,8 +1,13 @@
 package com.ufape.finproservice.repository;
 
+import com.ufape.finproservice.dto.MonthlyIncomeSumDTO;
 import com.ufape.finproservice.model.Income;
+import com.ufape.finproservice.model.UserEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -23,4 +28,18 @@ public interface IncomeRepository extends JpaRepository<Income, Long>, JpaSpecif
 
     List<Income> findByUserIdAndIncomeCategoryId(Long userId, Long incomeCategoryId);
 
+    @Query("""
+        select new com.ufape.finproservice.dto.MonthlyIncomeSumDTO(
+            year(i.date), month(i.date), sum(i.amount
+        )
+        from Income i
+        where i.user = :user
+          and (:year is null or year(i.date) = :year)
+        group by year(i.date), month(i.date)
+        order by year(i.date), month(i.date)
+        """)
+    List<MonthlyIncomeSumDTO> sumByMonth(
+            @Param("user") UserEntity user,
+            @Param("year") Integer year
+    );   
 }

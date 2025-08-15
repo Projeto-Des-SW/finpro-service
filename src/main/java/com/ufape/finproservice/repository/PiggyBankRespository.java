ae62817c809a3f0a187dee2983419f8f7e176e79
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-15 01:12:17 -0300

    Refactor: Adição do JPA :pig:

--- src/main/java/com/ufape/finproservice/repository/PiggyBankRespository.java
+++ src/main/java/com/ufape/finproservice/repository/PiggyBankRespository.java
@@ -1,16 +1,17 @@
 package com.ufape.finproservice.repository;
 
 import com.ufape.finproservice.model.PiggyBank;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PiggyBankRespository {
+public interface PiggyBankRespository extends JpaRepository<PiggyBank, Long> {
     List<PiggyBank> findByUserId(Long userId);
 
     boolean existsByNameAndUserId(String name, Long userId);
 
-    Optional<PiggyBank> findByPiggyBankIdAndUserID(Long piggyBankId, Long userId);
+    Optional<PiggyBank> findByPiggyBankIdAndUserId(Long piggyBankId, Long userId);
 
     List<PiggyBank> findByNameContainingIgnoreCaseAndUserId(String name, Long userId);
 

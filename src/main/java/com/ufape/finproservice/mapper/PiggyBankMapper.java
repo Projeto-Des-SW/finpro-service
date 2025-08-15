99a65237ea66bc8c83888caafe62e9850645b690
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-14 23:52:30 -0300

    Feat: Mapper para o porquinho :pig:

--- src/main/java/com/ufape/finproservice/mapper/PiggyBankMapper.java
+++ src/main/java/com/ufape/finproservice/mapper/PiggyBankMapper.java
@@ -1,6 +1,35 @@
 package com.ufape.finproservice.mapper;
 
+import com.ufape.finproservice.dto.PiggyBankDTO;
+import com.ufape.finproservice.dto.PiggyBankResponseDTO;
+import com.ufape.finproservice.model.PiggyBank;
+import com.ufape.finproservice.model.UserEntity;
+
 public class PiggyBankMapper {
 
+    public static PiggyBank toEntity(PiggyBankDTO dto, UserEntity user) {
+        if (dto == null) return null;
+
+        return PiggyBank.builder()
+                .name(dto.getName())
+                .status(dto.getStatus())
+                .monthlyDeposit(dto.getMonthlyDeposit())
+                .savingsGoal(dto.getSavingsGoal())
+                .targetDate(dto.getTargetDate())
+                .build();
+    }
+
+    public static PiggyBankResponseDTO piggyBankResponseDTO(PiggyBank piggyBank) {
+        if (piggyBank == null) return null;
+
+        return PiggyBankResponseDTO.builder()
+                .piggyBankId(piggyBank.getPiggyBankId())
+                .name(piggyBank.getName())
+                .monthlyDeposit(piggyBank.getMonthlyDeposit())
+                .savingsGoal(piggyBank.getSavingsGoal())
+                .targetDate(piggyBank.getTargetDate())
+                .status(piggyBank.getStatus())
+                .build();
+    }
 
 }

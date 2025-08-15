02ec68405367dd1d65f9250e7827708334625c24
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-14 22:59:06 -0300

    Feat: Feat: Criar entidade para o porquinho :pig:

---
+++ src/main/java/com/ufape/finproservice/model/PiggyBank.java
@@ -0,0 +1,37 @@
+package com.ufape.finproservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "piggy_bank")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PiggyBank {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long piggyBankId;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal savingsGoal;
+
+    @Column(nullable = false)
+    private LocalDate targetDate;
+
+    @Column(nullable = false)
+    private BigDecimal monthlyDeposit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+}

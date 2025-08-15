db1291e00f583180a052d49d4a8bc3e91aacd6e0
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-15 00:10:23 -0300

    Feat: Repository para o cofrinho :pig:

--- src/main/java/com/ufape/finproservice/repository/PiggyBankRespository.java
+++ src/main/java/com/ufape/finproservice/repository/PiggyBankRespository.java
@@ -1,4 +1,17 @@
 package com.ufape.finproservice.repository;
 
+import com.ufape.finproservice.model.PiggyBank;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface PiggyBankRespository {
+    List<PiggyBank> findByUserId(Long userId);
+
+    boolean existsByNameAndUserId(String name, Long userId);
+
+    Optional<PiggyBank> findByPiggyBankIdAndUserID(Long piggyBankId, Long userId);
+
+    List<PiggyBank> findByNameContainingIgnoreCaseAndUserId(String name, Long userId);
+
 }

53beca3bef0d5f8d9f31d4042b3ffdce728cb46f
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-15 01:14:34 -0300

    Feat: Controller do porquinho :pig:

--- src/main/java/com/ufape/finproservice/controller/PiggyBankController.java
+++ src/main/java/com/ufape/finproservice/controller/PiggyBankController.java
@@ -1,4 +1,23 @@
 package com.ufape.finproservice.controller;
 
+import com.ufape.finproservice.dto.PiggyBankDTO;
+import com.ufape.finproservice.dto.PiggyBankResponseDTO;
+import com.ufape.finproservice.service.PiggyBankService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/piggy-bank")
+@AllArgsConstructor
 public class PiggyBankController {
+
+    private final PiggyBankService piggyBankService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PiggyBankResponseDTO createPiggyBank(@RequestBody @Valid PiggyBankDTO piggyBankDTO) {
+        return piggyBankService.createPiggyBank(piggyBankDTO);
+    }
 }

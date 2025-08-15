7b536eb9442ddd5190f3e4476a022fa2bbb4d3b3
TayaneCibely <tayane.cibely@ufape.edu.br>
2025-08-14 23:39:42 -0300

    Feat: DTO e ResponseDTO do porquinho :pig:

--- src/main/java/com/ufape/finproservice/dto/PiggyBankDTO.java
+++ src/main/java/com/ufape/finproservice/dto/PiggyBankDTO.java
@@ -1,4 +1,18 @@
 package com.ufape.finproservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@Builder
 public class PiggyBankDTO {
+    private String name;
+    private String status;
+    private BigDecimal monthlyDeposit;
+    private BigDecimal savingsGoal;
+    private LocalDate targetDate;
 }
--- src/main/java/com/ufape/finproservice/dto/PiggyBankResponseDTO.java
+++ src/main/java/com/ufape/finproservice/dto/PiggyBankResponseDTO.java
@@ -1,4 +1,20 @@
 package com.ufape.finproservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@Builder
 public class PiggyBankResponseDTO {
+    private Long piggyBankId;
+    private String name;
+    private BigDecimal monthlyDeposit;
+    private BigDecimal savingsGoal;
+    private LocalDate targetDate;
+    private String status;
 }

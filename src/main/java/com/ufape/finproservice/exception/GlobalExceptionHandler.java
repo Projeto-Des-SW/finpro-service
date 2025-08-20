591ee9b27afcb7a45f56a652c5ad6a12a6761c06
mymph <leticialivia7991@gmail.com>
2025-08-20 01:05:48 -0300

    feat: tratamento específico para AccessDeniedException em GlobalExceptionHandler

--- src/main/java/com/ufape/finproservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ufape/finproservice/exception/GlobalExceptionHandler.java
@@ -2,10 +2,20 @@ package com.ufape.finproservice.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ApiErrorResponse> handleAccessDenied(AccessDeniedException ex) {
+        ApiErrorResponse error = new ApiErrorResponse(
+                "ACCESS_DENIED",
+                ex.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(error);
+    }
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ApiErrorResponse> customException(CustomException ex) {

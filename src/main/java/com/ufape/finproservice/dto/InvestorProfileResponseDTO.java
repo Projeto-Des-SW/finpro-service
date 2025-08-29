42d81301bf33fe6719a9b43f5062fab047720e22
mymph <leticialivia7991@gmail.com>
2025-08-28 23:53:14 -0300

    feat: criar DTO InvestorProfileResponseDTO com campos de resultado do perfil

---
+++ src/main/java/com/ufape/finproservice/dto/InvestorProfileResponseDTO.java
@@ -0,0 +1,26 @@
+package com.ufape.finproservice.dto;
+
+import java.time.LocalDateTime;
+
+import com.ufape.finproservice.enumeration.InvestmentTerm;
+import com.ufape.finproservice.enumeration.KnowledgeLevel;
+import com.ufape.finproservice.enumeration.RiskProfile;
+import com.ufape.finproservice.enumeration.RiskTolerance;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class InvestorProfileResponseDTO {
+    private Long id;
+    private RiskProfile riskProfile;
+    private Integer score;
+    private RiskTolerance riskTolerance;
+    private InvestmentTerm investmentTerm;
+    private KnowledgeLevel knowledgeLevel;
+    private LocalDateTime createdAt;
+    private Long userId;
+}

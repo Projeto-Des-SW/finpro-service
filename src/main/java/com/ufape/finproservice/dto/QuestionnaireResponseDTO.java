b99bed6ecbb1b13b4641ed29d11b041f925becde
mymph <leticialivia7991@gmail.com>
2025-08-28 23:50:43 -0300

    feat: criar DTO QuestionnaireResponseDTO com lista de QuestionAnswerDTO

---
+++ src/main/java/com/ufape/finproservice/dto/QuestionnaireResponseDTO.java
@@ -0,0 +1,18 @@
+package com.ufape.finproservice.dto;
+
+import java.util.List;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class QuestionnaireResponseDTO {
+    @NotEmpty
+    @Valid
+    private List<QuestionAnswerDTO> answers;
+}

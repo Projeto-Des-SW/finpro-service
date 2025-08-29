c7a5ff4c85c602d696f823bd4933aa7c1a220c2e
mymph <leticialivia7991@gmail.com>
2025-08-28 23:59:04 -0300

    feat: adicionar InvestorProfileController com endpoints para calcular, salvar, atualizar e buscar perfil do investidor

---
+++ src/main/java/com/ufape/finproservice/controller/InvestorProfileController.java
@@ -0,0 +1,50 @@
+package com.ufape.finproservice.controller;
+
+import com.ufape.finproservice.dto.InvestorProfileRequestDTO;
+import com.ufape.finproservice.dto.InvestorProfileResponseDTO;
+import com.ufape.finproservice.dto.ProfileCalculationResultDTO;
+import com.ufape.finproservice.dto.QuestionnaireResponseDTO;
+import com.ufape.finproservice.service.InvestorProfileService;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/investor-profile")
+@AllArgsConstructor
+public class InvestorProfileController {
+    
+    private final InvestorProfileService investorProfileService;
+    
+    @PostMapping("/calculate")
+    public ResponseEntity<ProfileCalculationResultDTO> calculateProfile(
+            @Valid @RequestBody QuestionnaireResponseDTO questionnaireDTO) {
+        ProfileCalculationResultDTO result = investorProfileService.calculateProfile(questionnaireDTO);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping
+    public ResponseEntity<InvestorProfileResponseDTO> saveProfile(
+            @Valid @RequestBody InvestorProfileRequestDTO requestDTO) {
+        InvestorProfileResponseDTO response = investorProfileService.saveProfile(requestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    
+    @GetMapping
+    public ResponseEntity<InvestorProfileResponseDTO> getProfile() {
+        InvestorProfileResponseDTO response = investorProfileService.getCurrentUserProfile();
+        return ResponseEntity.ok(response);
+    }
+    
+    @PutMapping
+    public ResponseEntity<InvestorProfileResponseDTO> updateProfile(
+            @Valid @RequestBody InvestorProfileRequestDTO requestDTO) {
+        InvestorProfileResponseDTO response = investorProfileService.saveProfile(requestDTO);
+        return ResponseEntity.ok(response);
+    }
+}

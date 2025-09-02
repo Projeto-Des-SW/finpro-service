a4b068d56e2e3b2e1df1851ef8816ae8452fb38c
izabel <izabel.nascimento@nees.ufal.br>
2025-09-02 00:28:17 -0300

    refactor: corrigindo nome da classe PiggyBankRepository

--- src/main/java/com/ufape/finproservice/service/PiggyBankService.java
+++ src/main/java/com/ufape/finproservice/service/PiggyBankService.java
@@ -9,7 +9,7 @@ import com.ufape.finproservice.exception.ExceptionMessage;
 import com.ufape.finproservice.mapper.PiggyBankMapper;
 import com.ufape.finproservice.model.PiggyBank;
 import com.ufape.finproservice.model.UserEntity;
-import com.ufape.finproservice.repository.PiggyBankRespository;
+import com.ufape.finproservice.repository.PiggyBankRepository;
 import com.ufape.finproservice.util.CurrentUserService;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
@@ -26,7 +26,7 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 public class PiggyBankService {
 
-    private final PiggyBankRespository piggyBankRepository;
+    private final PiggyBankRepository piggyBankRepository;
     private final CurrentUserService currentUserService;
 
     @Transactional
